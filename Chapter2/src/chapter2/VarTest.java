a7edefc6991fa8b8ab1ebbee0104685f61a1260e
kimmingyu <soil0201@gmail.com>
2022-02-02 22:42:05 +0900

    Variable Test

---
+++ Chapter2/src/chapter2/VarTest.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class VarTest {
+
+	int	abc;
+	public static void main(String[] args) {
+
+		var dNum = 3.14;
+		
+		System.out.println(dNum);
+		
+		var num = 10;
+		System.out.println(num);
+	}
+
+}
---
+++ Chapter2/src/chapter2/VariableEx2.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class VariableEx2 {
+
+	public static void main(String[] args) {
+		int	num = 10;
+		int	num2 = 2147483647;
+		long num3 = 2147483648L;
+		
+		
+		System.out.println(num);
+		System.out.println(num2);
+		System.out.println(num3);
+	}
+
+}
