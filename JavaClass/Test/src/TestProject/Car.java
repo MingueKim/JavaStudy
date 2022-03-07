2265e9d828cdf4ecf75918d46c9a53fbc6f0e556
kimmingyu <soil0201@gmail.com>
2022-03-07 19:39:08 +0900

    Class Test

---
+++ JavaClass/Test/src/TestProject/Car.java
@@ -0,0 +1,19 @@
+package TestProject;
+
+public class Car {
+    public String color;
+    public String gear;
+    public int price;
+
+    public Car() {
+        System.out.println("Car");
+    }
+
+    public void run() {
+        System.out.println("run");
+    }
+
+    public void stop() {
+        System.out.println("stop");
+    }
+}
